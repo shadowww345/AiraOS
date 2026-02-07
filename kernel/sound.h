a400b1df419b59569059d35dfa8a52f75e7dc32a
shadow <acaromer330@gmail.com>
2026-02-07 09:38:29 +0300

    Update sound.h

--- kernel/sound.h
+++ kernel/sound.h
@@ -4,6 +4,22 @@
 typedef unsigned int   uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char  uint8_t;
+extern uint8_t* sound_buffer;
+typedef struct {
+    char     riff[4];
+    uint32_t overall_size;
+    char     wave[4];
+    char     fmt_chunk_marker[4];
+    uint32_t length_of_fmt;
+    uint16_t format_type;
+    uint16_t channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t bits_per_sample;
+    char     data_chunk_header[4];
+    uint32_t data_size;
+} __attribute__((packed)) WAV_Header;
 
 //\\ --Sound Blaster 16-- \\//
 int reset_sb16();
@@ -15,8 +31,9 @@ void setup_dma(uint32_t buffer_addr, uint32_t length);
 void play_sound(unsigned int nFrequence);
 void stop_sound();
 void beep();
-void beep_with_duration(unsigned int freq, int duration);
-void beep_freq(unsigned int freq);
-
+void beep_with_duration(int freq, int duration);
+void beep_freq(int freq);
+void prepare_audio();
+void play_wav(uint8_t* wav_data);
 
 #endif
