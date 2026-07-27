f3286e45f7b9fb4dd61aa0b5e637f7384681a383
shadow <acaromer330@gmail.com>
2026-07-27 03:50:59 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -116,7 +116,8 @@ void kernel_main() {
     print(aira_os);
     draw_status_bar();
     print("Welcome To AiraOS!\n");
-    print("Live-Session-User");
+    print(username);
+    print("$");
     outb(0x3D4, 0x0A);
     outb(0x3D5, (inb(0x3D5) & 0xC0) | 0);
     outb(0x3D4, 0x0B);
@@ -280,7 +281,7 @@ void kernel_main() {
                     }
                     else if (idx > 0) print("Undefined Command\n");
                     
-                    idx = 0; print("Live-Session-User");
+                    idx = 0; print(username); print("$");
                 } else if (c == '\b' && idx > 0) {
                     idx--; put_char('\b');
                 } else if (c != 0 && idx < 63) {
